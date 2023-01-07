62f7eb1d4193d45be6a5a20bff759a0bfd0456f8
Rishabh Shukla <shuklarishabh890@gmail.com>
2023-01-07 13:40:43 +0530

    Added users and posts controller and routes

--- routes/r_index.js
+++ routes/r_index.js
@@ -5,6 +5,7 @@ const homeController = require('../controllers/home_controller')
 console.log('loaded routes')
 
 router.get('/', homeController.home);
-
+router.use('/users', require('./users'))
+router.use('/users', require('./posts'))
 
 module.exports = router;
